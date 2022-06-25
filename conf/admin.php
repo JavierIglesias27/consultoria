bb2ba67c706896e18078635a82626a5f490e21ba
JavierIglesias27 <jiglesro7@gmail.com>
2022-06-25 14:19:05 +0200

    save24

--- conf/admin.php
+++ conf/admin.php
@@ -1,6 +1,8 @@
 <?php
 
-define("RECAPTCHA_V3_SECRET_KEY", '6LepHlMgAAAAANAWwdPTbXISe5rKHLbQEno8tQV1');
+ define("RECAPTCHA_V3_SECRET_KEY", '6LepHlMgAAAAANAWwdPTbXISe5rKHLbQEno8tQV1');
+
+ // las 4 primeras van  localhost/phpMYadmin
 
 define("data_base_hosting_consultoria", 'localhost');
 
@@ -9,6 +11,17 @@ define("data_base_username_consultoria", 'root');
 define("data_base_password_consultoria", '');
 
 define("nameTabla_data_base_consultoria", 'pbd');
+
+
+//estas van al freemysqlhosting.met q hay q renovar casa semana
+
+// define("data_base_hosting_consultoria", 'sql4.freemysqlhosting.net');
+
+// define("data_base_username_consultoria", 'sql4501016');
+
+// define("data_base_password_consultoria", 'LNnLKKSRBe');
+
+// define("nameTabla_data_base_consultoria", 'sql4501016');
 
 //INTRODUCIR DATOS DE SQLMY ADMIN
 //https://www.freemysqlhosting.net/register/?action=register
