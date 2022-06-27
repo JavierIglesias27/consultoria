e0b65237f7218e2dfdfc28bc961d6b265cd11d67
JavierIglesias27 <jiglesro7@gmail.com>
2022-06-27 15:08:36 +0200

    save

--- conf/admin.php
+++ conf/admin.php
@@ -5,26 +5,26 @@
  // las 4 primeras van  localhost/phpMYadmin
  // cambiar puerto en registrase php  $PortSMTP = 465; // con consulting.localhost este
 
-define("data_base_hosting_consultoria", 'localhost');
+// define("data_base_hosting_consultoria", 'localhost');
 
-define("data_base_username_consultoria", 'root');
+// define("data_base_username_consultoria", 'root');
 
-define("data_base_password_consultoria", '');
+// define("data_base_password_consultoria", '');
 
-define("nameTabla_data_base_consultoria", 'pbd');
+// define("nameTabla_data_base_consultoria", 'pbd');
 
 
 //estas van al freemysqlhosting.met q hay q renovar casa semana
 // jay q ir a registrarse php y cambiar el puerto a $PortSMTP = 587; // freeemyhosting: este puerto
 //linea 120 +o-
 
-// define("data_base_hosting_consultoria", 'sql4.freemysqlhosting.net');
+define("data_base_hosting_consultoria", 'sql4.freemysqlhosting.net');
 
-// define("data_base_username_consultoria", 'sql4501016');
+define("data_base_username_consultoria", 'sql4501016');
 
-// define("data_base_password_consultoria", 'LNnLKKSRBe');
+define("data_base_password_consultoria", 'LNnLKKSRBe');
 
-// define("nameTabla_data_base_consultoria", 'sql4501016');
+define("nameTabla_data_base_consultoria", 'sql4501016');
 
 //INTRODUCIR DATOS DE SQLMY ADMIN
 //https://www.freemysqlhosting.net/register/?action=register
