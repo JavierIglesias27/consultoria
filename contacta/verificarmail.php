51432f10b1777168f6b1e45b148575bcb5962143
javierIglesias27 <jiglesro7@gmail.com>
2022-06-27 09:15:22 +0200

    save

--- contacta/verificarmail.php
+++ contacta/verificarmail.php
@@ -119,7 +119,7 @@
                             </div>
                             <!-- Callout Panel -->
                             <p class="callout" >
-                               Regresa a la página principal <a href="' .  $_SERVER['HTTP_HOST'] . '/index.html">Click aquí! &raquo;</a>
+                               Regresa a la página principal <a href="./index.html">Click aquí! &raquo;</a>
                             </p><!-- /Callout Panel -->					
                                                     
                             <!-- social & contact -->
